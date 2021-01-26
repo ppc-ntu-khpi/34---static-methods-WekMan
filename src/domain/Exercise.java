7759298940c90b687b9b37227898b2a8e6e318f2
WekMan <58940879+WekMan@users.noreply.github.com>
2021-01-26 23:59:32 +0200

    Add files via upload

---
+++ src/domain/Exercise.java
@@ -0,0 +1,22 @@
+package domain;
+
+/**
+ * Class that callculates palindrome
+ * @author Artem
+ */
+public class Exercise {
+    
+    /**
+     * Method returns whether the string is a palindrome
+     * @param string string that will be check
+     * @return boolean(true: if string is palindrome, false: if string isn`t palindrome)
+     */
+    public static boolean IsPalindrome(String string){
+        
+        string = string.replace(" ", "");
+        
+        String reverseString = new StringBuilder(string).reverse().toString();
+
+        return reverseString.equalsIgnoreCase(string);
+    }
+}
